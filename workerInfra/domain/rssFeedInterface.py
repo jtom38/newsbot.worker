d95ad5710eb1017b20c213f9d136d1bf4e16f157
James Tombleson <luther38@gmail.com>
2022-01-03 07:35:18 -0800

    Jtom38/issue13 (#19)
    * added a more defined rss feed interface
    
    * added doc strings

--- workerInfra/domain/rssFeedInterface.py
+++ workerInfra/domain/rssFeedInterface.py
@@ -1,20 +1,58 @@
 from abc import ABC, abstractclassmethod
-from typing import List
+from workerInfra.domain.loggerInterface import LoggerInterface
+from workerInfra.models.dbModels import Articles
+from bs4 import BeautifulSoup
+from typing import Dict, List, Union
 
 
 class RssFeedInterface(ABC):
-    @abstractclassmethod
-    def checkSiteIcon(self) -> None:
-        raise NotImplementedError()
+    _logger: LoggerInterface
+    _url: str
+    _siteName: str
+    _sourceId: str
+    _soup: Union[BeautifulSoup, Dict]
 
     @abstractclassmethod
-    def getPosts(self) -> List:
-        raise NotImplementedError()
+    def collectItems(self) -> List[BeautifulSoup]:
+        """This collects all the items from the feed."""
+        ...
 
     @abstractclassmethod
-    def findFeedTitle(self) -> str:
-        raise NotImplementedError()
+    def processItem(self, soup: BeautifulSoup) -> Articles:
+        """Process a single entry from the feed."""
+        ...
 
     @abstractclassmethod
-    def findItemLink(self) -> str:
-        raise NotImplementedError()
+    def getTitle(self) -> str:
+        """Gets the title from the stored soup object against known values."""
+        ...
+
+    @abstractclassmethod
+    def getAuthorName(self) -> str:
+        """Gets the author name from the stored soup object against known values."""
+        ...
+
+    @abstractclassmethod
+    def getPublishDate(self) -> str:
+        """Gets the publish date from the stored soup object against known values."""
+        ...
+
+    @abstractclassmethod
+    def getDescription(self) -> str:
+        """Gets the description/content from the stored soup object against known values."""
+        ...
+
+    @abstractclassmethod
+    def getThumbnail(self) -> str:
+        """Gets the thumbnail from the stored soup object against known values."""
+        ...
+
+    @abstractclassmethod
+    def getTags(self) -> str:
+        """Searches the _soup object to find any tags by a defined list."""
+        ...
+
+    @abstractclassmethod
+    def getLink(self) -> str:
+        """Searches the _soup object to find the link object."""
+        ...
