813b700381c0b1f6a44516ffe3a389df1747b029
James Tombleson <luther38@gmail.com>
2022-01-06 16:18:44 -0800

    updated how the sources handle in debug and spaced out when they will pull information to avoid high cpu load (#22)

--- workerApi/apiEventsService.py
+++ workerApi/apiEventsService.py
@@ -31,13 +31,13 @@ class ApiEventsService():
         pass
 
     def startup(self) -> None:
-        # self._scheduler.addJob(self.enableSourceReddit())
-        # self._scheduler.addJob(self.enableSourceYoutube())
-        # self._scheduler.addJob(self.enableSourceTwitter())
-        # self._scheduler.addJob(self.enableSourceTwitch())
-        # self._scheduler.addJob(self.enableSourcePokemonGo())
-        # self._scheduler.addJob(self.enableSourceFFXIV())
-        # self._scheduler.addJob(self.enableSourceRss())
+        self._scheduler.addJob(self.enableSourceReddit())
+        self._scheduler.addJob(self.enableSourceYoutube())
+        self._scheduler.addJob(self.enableSourceTwitter())
+        self._scheduler.addJob(self.enableSourceTwitch())
+        self._scheduler.addJob(self.enableSourcePokemonGo())
+        self._scheduler.addJob(self.enableSourceFFXIV())
+        self._scheduler.addJob(self.enableSourceRss())
 
         self._scheduler.addJob(self.enableOutputDiscord())
 
@@ -46,31 +46,31 @@ class ApiEventsService():
     def enableSourceRss(self) -> SchedulerJobModel:
         s = SchedulerJobModel(functionName=WorkerService(RssWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.RSSENABLED) is False:
             s.enabled = False
         return s
 
     def enableSourceReddit(self) -> SchedulerJobModel:
-        s = SchedulerJobModel(functionName=WorkerService(RedditWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
+        s = SchedulerJobModel(functionName=WorkerService(RedditWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=25)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.REDDITENABLED) is False:
             s.enabled = False
         return s
 
     def enableSourceYoutube(self) -> SchedulerJobModel:
-        s = SchedulerJobModel(functionName=WorkerService(YoutubeWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
+        s = SchedulerJobModel(functionName=WorkerService(YoutubeWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=35)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.YOUTUBEENABLED) is False:
             s.enabled = False
         return s
 
     def enableSourceTwitter(self) -> SchedulerJobModel:
-        s = SchedulerJobModel(functionName=WorkerService(TwitterWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
+        s = SchedulerJobModel(functionName=WorkerService(TwitterWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=37)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.TWITCHEANBLED) is False:
             s.enabled = False
         return s
@@ -78,23 +78,23 @@ class ApiEventsService():
     def enableSourceTwitch(self) -> SchedulerJobModel:
         s = SchedulerJobModel(functionName=WorkerService(TwitchWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.TWITCHEANBLED) is False:
             s.enabled = False
         return s
 
     def enableSourcePokemonGo(self) -> SchedulerJobModel:
-        s = SchedulerJobModel(functionName=WorkerService(PokemonGoWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
+        s = SchedulerJobModel(functionName=WorkerService(PokemonGoWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=35)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.POKEMONGOENABLED) is False:
             s.enabled = False
         return s
 
     def enableSourceFFXIV(self) -> SchedulerJobModel:
-        s = SchedulerJobModel(functionName=WorkerService(FFXIVWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=30)
+        s = SchedulerJobModel(functionName=WorkerService(FFXIVWorkerService()).init, trigger=SchedulerTriggerEnum.INTERVAL, minutes=45)
         if self._isDebug is True:
-            s.minutes = 1
+            s.trigger = SchedulerTriggerEnum.NONE
         if self._env.getValue(EnvEnum.FFXIVENABLED) is False:
             s.enabled = False
         return s
